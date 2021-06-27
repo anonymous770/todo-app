d12e9d487ddc11c43ee6decd7e2cb4d94fe229eb
anonymous770 <aslammiya5257@gmail.com>
2021-06-27 15:48:54 +0000

    03:48:54 PM

--- app.py
+++ app.py
@@ -8,46 +8,47 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key = True) 
-    work = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(1000), nullable=False)
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f"{self.sno} - {self.work}"
+        return f"{self.sno} - {self.title}"
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def hello_world():
     if request.method=='POST':
-        work = request.form['work']
+        title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(work=work, desc=desc)
+        todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
-    alltodo = Todo.query.all()
-    return render_template('index.html', alltodo=alltodo)
+        
+    allTodo = Todo.query.all() 
+    return render_template('index.html', allTodo=allTodo)
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
     if request.method=='POST':
-        work = request.form['work']
+        title = request.form['title']
         desc = request.form['desc']
         todo = Todo.query.filter_by(sno=sno).first()
-        todo.work = work
+        todo.title = title
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
-        return redirect('/')
+        return redirect("/")
+        
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
-@app.route("/delete/<int:sno>")
+@app.route('/delete/<int:sno>')
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect("/")    
+    return redirect("/")
 
-if __name__ == '__main__':
-    app.run(debug=True, port=9000)
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
